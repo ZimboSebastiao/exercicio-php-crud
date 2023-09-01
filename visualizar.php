187eaaca55332bc849d702998d4ccbc7d0154d33
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-08-31 21:04:01 -0300

    exercicio concluido

--- visualizar.php
+++ visualizar.php
@@ -52,7 +52,7 @@ $listaDeAlunos = lerAlunos($conexao);
     <nav class="navbar bg-body-tertiary">
     <div class="container-fluid">
         <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Digite para pesquisar um aluno" aria-label="Search">
+        <input class="form-control me-2 pesquisa" type="search" placeholder="Digite para pesquisar um aluno" aria-label="Search">
         <!-- <button class="btn btn-outline-success" type="submit">Search</button> -->
         </form>
     </div>
